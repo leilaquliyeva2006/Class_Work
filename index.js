36c5d91d793f11febb9fc3f012036e1621913497
leila <leylag659@gmail.com>
2023-11-30 20:23:59 +0400

    cerez target

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const btns = document.querySelectorAll('button');
 
-btns[0].addEventListener('click', function(){
-  btns[0].style.color = 'red'
+
+btns[0].addEventListener('click', function(event){
+ event.target.style.color = 'red'
 })
-btns[1].addEventListener('click', function(){
-  btns[1].style.backgroundColor = 'green'
+btns[1].addEventListener('click', function(event){
+  event.target.style.backgroundColor = 'green'
 })
