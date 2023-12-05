9ec96fca7e269ad323514977eaed9eb0e98e170e
leila <leylag659@gmail.com>
2023-12-05 19:47:19 +0400

    отключить перезагрузку страницы при нажатии на кнопку

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-const btns = document.querySelectorAll("button");
-
-const div = document.querySelector("div")
-
-
-function changeStyle(event){
-  if(event.target!=div){
-event.target.style.color = 'green'
-  }
-  
-  console.log(event.target)
-}
-
-
-
-div.addEventListener('click',changeStyle)
+document.getElementById('myForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+});
