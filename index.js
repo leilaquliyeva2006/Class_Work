e1ef93ff080b886d306eec641eb1ca040cc1992d
leila <leylag659@gmail.com>
2023-12-12 21:28:57 +0400

    pakman s canvas

--- index.js
+++ index.js
@@ -6,20 +6,20 @@ const ctx = canvas.getContext("2d");
 canvas.width = CANVAS_WIDTH;
 canvas.height = CANVAS_HEIGHT;
 
-ctx.lineWidth = 10;
+ctx.lineWidth = 3;
+ctx.strokeStyle = "black";
+ctx.fillStyle = "red";
 
-//Fill edges
-ctx.strokeRect(75, 140, 150, 110);
-
-// Fill inside
-ctx.fillRect(130, 190, 40, 60);
-
-//Drow triangle
 ctx.beginPath();
-ctx.moveTo(50, 140);
-ctx.lineTo(150, 60);
-ctx.lineTo(250, 140);
-ctx.closePath()
 
-
+ctx.arc(
+   100,
+   100,
+   100,
+   0.2 * Math.PI,
+   1.8 * Math.PI
+   );
+ctx.lineTo(100, 100);
+ctx.closePath();
+ctx.fill();
 ctx.stroke();
