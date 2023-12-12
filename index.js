184f16fae607de053e415e5aa5cc867423311dff
leila <leylag659@gmail.com>
2023-12-12 19:41:44 +0400

    Canvas

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const textarea = document.querySelector('textarea')
-const span = document.querySelector('span')
+const CANVAS_WIDTH = 1000;
+const CANVAS_HEIGHT = 800;
 
 
-textarea.addEventListener('input',function(event){
-  const length = event.target.value.length
-  span.innerText = 'Count symbol ' + length
-})
+
+const canvas = document.querySelector('canvas')
+const ctx = canvas.getContext('2d')
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT
 
 
+ctx.fillStyle = 'green';
+ctx.fillRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT)
