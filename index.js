e849a08c0c1360545483160c91904f3ff268049a
leila <leylag659@gmail.com>
2023-12-12 19:55:54 +0400

    domik s canvas

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const CANVAS_WIDTH = 1000;
 const CANVAS_HEIGHT = 800;
 
-
-
-const canvas = document.querySelector('canvas')
-const ctx = canvas.getContext('2d')
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
 canvas.width = CANVAS_WIDTH;
-canvas.height = CANVAS_HEIGHT
+canvas.height = CANVAS_HEIGHT;
+
+ctx.lineWidth = 10;
+
+//Fill edges
+ctx.strokeRect(75, 140, 150, 110);
+
+// Fill inside
+ctx.fillRect(130, 190, 40, 60);
+
+//Drow triangle
+ctx.beginPath();
+ctx.moveTo(50, 140);
+ctx.lineTo(150, 60);
+ctx.lineTo(250, 140);
+ctx.closePath()
 
 
-ctx.fillStyle = 'green';
-ctx.fillRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT)
+ctx.stroke();
