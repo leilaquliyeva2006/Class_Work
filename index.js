7061d0a65c2932ac833c3f5c99fe01a4651dde82
leila <leylag659@gmail.com>
2023-12-26 20:05:09 +0400

    pattern fabricniy metod

--- index.js
+++ index.js
@@ -1,25 +1,67 @@
-const CANVAS_WIDTH = 1000;
-const CANVAS_HEIGHT = 800;
+//v JS netu primitivnix i ne primitivnix tipov dannix vse tipi dannix xranatsa po sillke
 
-const canvas = document.querySelector("canvas");
-const ctx = canvas.getContext("2d");
-canvas.width = CANVAS_WIDTH;
-canvas.height = CANVAS_HEIGHT;
+const OS = "linux";
 
-ctx.lineWidth = 3;
-ctx.strokeStyle = "black";
-ctx.fillStyle = "red";
+class AbstractButton {
+  #elemnet;
 
-ctx.beginPath();
+  constructor() {
+    if (new.target === AbstractButton)
+      throw new Error("Can't create instance of abstract class");
 
-ctx.arc(
-   100,
-   100,
-   100,
-   0.2 * Math.PI,
-   1.8 * Math.PI
-   );
-ctx.lineTo(100, 100);
-ctx.closePath();
-ctx.fill();
-ctx.stroke();
+    const btn = document.createElement("button");
+
+    this.#elemnet = btn;
+  }
+  get element() {
+    return this.#elemnet;
+  }
+}
+
+class windowsButton extends AbstractButton {
+  constructor() {
+    super();
+    this.element.textContent = "Windows button";
+  }
+  click() {
+    console.log("You cliked " + this.textContent);
+  }
+}
+class MacButton extends AbstractButton {
+  constructor() {
+    super();
+    this.element.textContent = "Mac button";
+  }
+  click() {
+    console.log("You cliked " + this.textContent);
+  }
+}
+class LinuxButton extends AbstractButton {
+  constructor() {
+    super();
+    this.element.textContent = "Linux button";
+  }
+  click() {
+    console.log("You cliked " + this.textContent);
+  }
+}
+
+class ButtonFactory {
+  static createButton() {
+    switch (OS) {
+      case "windows":
+        return new windowsButton();
+      case "mac":
+        return new MacButton();
+      case "linux":
+        return new LinuxButton();
+      default:
+        throw new Error("Unknow OS");
+    }
+  }
+}
+
+const btn = ButtonFactory.createButton();
+btn.element.addEventListener("click", btn.click);
+
+document.body.append(btn.element);
